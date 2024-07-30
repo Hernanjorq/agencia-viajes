5c6f777c23c02412c961792206ce41ea3a4a8c3f
furiarga <147948758+furiarga@users.noreply.github.com>
2024-07-29 22:21:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+nconst express = require('express');
 const mysql = require('mysql');
 const app = express();
 const port = 3000;
@@ -7,7 +7,7 @@ const port = 3000;
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Tupassword',
+  password: '12345',
   database: 'semana8'
 });
 
